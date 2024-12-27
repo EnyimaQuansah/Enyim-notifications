c07889de7b627d1e36bd8460a35f7cd32185725a
Enyimah Quansah <quansahenyima@gmail.com>
2024-12-27 17:55:31 +0000

    vercel json

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const { initConsumer } = require("./events/consumer");
+const express = require("express");
+const notificationRoutes = require("./routes/notificationRoutes");
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB:", err.message));
+
+// Routes
+app.use("/notifications", notificationRoutes);
+
+// Initialize RabbitMQ Consumers
+initConsumer();
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,24 +1,7 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-const { initConsumer } = require("./events/consumer");
-const express = require("express");
-const notificationRoutes = require("./routes/notificationRoutes");
+const app = require("./app");
 
-const app = express();
-app.use(express.json());
+const PORT = process.env.PORT;
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Failed to connect to MongoDB:", err.message));
-
-// Routes
-app.use("/notifications", notificationRoutes);
-
-// Initialize RabbitMQ Consumers
-initConsumer();
-
-const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
-  console.log(`Notification microservice running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
